7068721e25f126d14c740e138f1c8efa0e5016f6
rafasaal88 <rafasaal88@gmail.com>
2018-11-12 16:41:52 +0100

    update

---
+++ P4/ej3.sql
@@ -0,0 +1,15 @@
+Set Serveroutput On;
+Declare
+Apellidos Votantes.Nombrecompleto%Type;
+Correo Votantes.Email%Type;
+Begin
+
+Select Votantes.Email, Substr(Votantes.Nombrecompleto, Instr(Votantes.Nombrecompleto, ' '), length(votantes.nombrecompleto))
+Into Correo, Apellidos From Votantes Where Dni='30983712';
+
+
+
+Dbms_Output.Put_Line('Pepe' ||Apellidos);
+End;
+
+
