88d5b54cf05c11a40688c15f44c90d41dcc02a1b
rafasaal88 <rafasaal88@gmail.com>
2019-01-06 12:27:42 +0100

    Create ej7.sql

---
+++ P3/ej7.sql
@@ -0,0 +1,18 @@
+select votantes.nombrecompleto
+from votantes, 
+(
+    select consultas.votante, count(consultas.votante)
+    from consultas
+    having count(consultas.votante) > 
+    (
+        select avg(conteo)
+        from
+        (
+            select count(consultas.votante) as conteo
+            from consultas
+            group by consultas.votante
+        )
+    )
+    group by consultas.votante
+) subconsulta
+where votantes.dni=subconsulta.votante;
